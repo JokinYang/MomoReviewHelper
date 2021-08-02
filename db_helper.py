f59281b43830d0790f945390d23741ff832ee8a1
JokinYang <yangjun97@foxmail.com>
2021-08-02 22:43:02 +0800

    initiate repo

---
+++ db_helper.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker, session
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+engine = create_engine('sqlite:///database.sqlite')
+
+
+class Word(Base):
+    __tablename__ = 'word'
+    word = Column(String(), primary_key=True)
+    book = Column(String())
+    pron_official_avatar = Column(String())
+    pronunciation = Column(String())
+    interpretation = Column(String())
+    ranking = Column(String())
+    study_user_count = Column(String())
+    difficulty = Column(String())
+    acknowledge_rate = Column(String())
+    note_type = Column(String())
+    note_content = Column(String())
+    phrase = Column(String())
+
+
+Base.metadata.create_all(engine, checkfirst=True)
+
+Session = sessionmaker(bind=engine)
+
+
+
