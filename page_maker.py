162dfe8938526d4f425f78e17bb6ab77866f0bce
JokinYang <yangjun97@foxmail.com>
2021-08-29 15:36:17 +0800

    .

--- page_maker.py
+++ page_maker.py
@@ -1,6 +1,7 @@
 import datetime
 from db_helper import WordDetail
 from typing import List
+import random
 
 
 class PageGenerator:
@@ -16,6 +17,9 @@ class PageGenerator:
         self.timestamp = '{:%Y%m%d}'.format(self.date.date() - datetime.timedelta(delta))
 
         self.words = words
+        random_int = random.randint(0, 2 ** 16)
+        self.interpretation_class = '{}_{}'.format('interpretation', str(random_int))
+        self.details_class = '{}_{}'.format('details', str(random_int))
 
     def gen_md(self, output_path=None):
         output_path = output_path or '{}.md'.format(self.timestamp)
@@ -24,11 +28,31 @@ class PageGenerator:
 /*不显示details的三角符号*/
 details > summary::marker {
     display: none;
-    content: "";
+    content: none;
+}
+/*去掉外边框*/
+details summary{
+    outline:none;
+    cursor:pointer;/*鼠标放上去之后变成手型*/
+}
+/*去掉前面默认的小黑三角*/
+details summary::-webkit-details-marker{
+    display:none; 
 }
 </style>
 """
-        content = style + content
+        script = ("""
+<script>
+const details = document.querySelectorAll('.{d}');
+const translates = document.querySelectorAll('.{tr}');
+""".format(d=self.details_class, tr=self.interpretation_class) + """
+details.forEach((item, index) => item.addEventListener('toggle', () => {
+    if (item.open) {
+        translates[index].style.display = 'inline-block';
+    } else translates[index].style.display = 'none';
+}));
+</script>""")
+        content = style + script + content
         with open(output_path, 'w') as f:
             f.write(content)
         return output_path, content
@@ -42,15 +66,6 @@ details > summary::marker {
 
         return '\n'.join(items)
 
-    def _gen_item(self, word: WordDetail, highlight_word=True):
-        phrase_list = word.phrase_list
-        if not phrase_list:
-            return f'Not found the phrase of:{word.word}\n'
-        phrase_choose = max(phrase_list, key=lambda x: len(x[0]))
-        en, zh = phrase_choose
-        en = en.replace(word.word, f' **{word.word}** ')
-        return f'{en}  \n'
-
     def _gen_item_html(self, word: WordDetail, highlight_word=True):
         phrase_list = word.phrase_list
         if not phrase_list:
@@ -60,12 +75,14 @@ details > summary::marker {
         en = en.replace(word.word, f'<strong>{word.word}</strong>')
         i = """
 <div><h2 style="display: inline-block;margin-bottom: 0;margin-top: 0">{word}</h2>
-    <p style="display: inline-block; padding:0 .5em; margin: 0;">{phonetic}</p></div>
-<details>
+    <p style="display: inline-block; padding:0 .5em; margin: 0;font-family: monospace;">{phonetic}</p>
+    <p class="{inter_class}" style="display: inline-block; padding:0 .5em; margin: 0;">{interpretation}</p></div>
+<details class="{detail_class}">
     <summary style="color: #303030;">{en}</summary>
     {ch}
 </details>
 <hr style="padding-bottom: 0.5em;" />
-""".format(phonetic=word.pronunciation,
-           word=word.word, en=en, ch=ch)
+""".format(word=word.word, en=en, ch=ch,
+           phonetic=word.pronunciation, interpretation=word.interpretation,
+           inter_class=self.interpretation_class, detail_class=self.details_class)
         return i
